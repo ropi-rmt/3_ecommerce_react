f98d4039929f01cb503cd11ce97589aea40d084d
ropi-rmt <ropi.rmt@gmail.com>
2026-08-06 14:12:54 -0300

    comenzando con firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,17 @@
+/* // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiXLNIEMb2VdQkgdaatGBR7MPWewUucTg",
+  authDomain: "tienda-online-rl.firebaseapp.com",
+  projectId: "tienda-online-rl",
+  storageBucket: "tienda-online-rl.firebasestorage.app",
+  messagingSenderId: "345717926054",
+  appId: "1:345717926054:web:a9e7a5ca4e6beb5fb42c67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);*/
